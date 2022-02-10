4bb60f3d738010821b8fdea2795fb803f81b651b
Ethan Pippin <ethanpippin2343@gmail.com>
2022-02-10 16:24:38 -0700

    add initial courses

---
+++ campus.py
@@ -0,0 +1,28 @@
+class Campus:
+    def __init__(self, title, baseURL, semesters):
+        self.title = title
+        self.baseURL = baseURL
+        self.semesters = semesters
+
+mainCampus = Campus('MainCampus',
+                    'https://student.apps.utah.edu/uofu/stu/ClassSchedules/main',
+                    {
+                        'Spring2022': '1224',
+                        'Fall2021': '1218',
+                        'Summer2021': '1216',
+                        'Spring2021': '1214'
+                    })
+
+asiaCampus = Campus('AsiaCampus',
+                    'https://student.apps.utah.edu/uofu/stu/ClassSchedules/uac',
+                    {
+                        'Spring2022': '1224'
+                    })
+
+uOnlineCampus = Campus('UOnline',
+                        'https://student.apps.utah.edu/uofu/stu/ClassSchedules/online',
+                        {
+                            'Spring2022': '1224'
+                        })
+
+allCampuses = [mainCampus, asiaCampus, uOnlineCampus]
--- parser.py
+++ parser.py
@@ -1,23 +1,32 @@
+import json
+import os
 import re
 
 from bs4 import BeautifulSoup
 
+from campus import allCampuses
+
 from urllib import request
 from urllib.parse import quote
 
-class CourseParser:
+class SemesterParser:
 
-    def __init__(self, catalogNum):
-        self.baseURL = f'https://student.apps.utah.edu/uofu/stu/ClassSchedules/main/{catalogNum}/'
+    def __init__(self, campus, catalogName, catalogNum):
+        self.baseURL = f'{campus.baseURL}/{catalogNum}/'
+        self.campus = campus
+        self.catalogName = catalogName
         self.catalogNum = catalogNum
-        self.subjects = []
+        self.subjects = {}
         self.subjectCourses = {}
 
-    def start(self):
+    def parseSemester(self):
+
+        print(f'\nBeginning to parse semester: { self.catalogName }\nCampus: { self.campus.title }')
+
         homePage = self.getHomePage()
         self.parseHomePage(homePage)
 
-        for subject in self.subjects[:1]:
+        for subject in list(self.subjects.keys())[:1]:
             subjectAbbr = subject.split(' - ')[0]
             subjectCoursesHTML = self.getSubjectCoursesPage(subjectAbbr)
             self.parseSubjectCoursesPage(subject, subjectCoursesHTML)
@@ -34,8 +43,15 @@ class CourseParser:
     def parseHomePage(self, pageHTML):
         pageSoup = BeautifulSoup(pageHTML, 'html.parser')
 
+        self.title = pageSoup.findChildren('span')[2].string[3:]
+
         for subject in pageSoup.select('a[href*="class_list.html?subject"]'):
-            self.subjects.append(subject.string)
+            subjectToken = subject.string.split(' - ')
+            subjectAbbr = subjectToken[0].replace(' ', '-')
+            subjectTitle = subjectToken[1]
+            self.subjects[subjectAbbr] = subjectTitle
+
+        print("-- parsed home page")
 
     def getSubjectCoursesPage(self, subjectAbbr):
         subjectURL = self.baseURL + f'class_list.html?subject={quote(subjectAbbr)}'
@@ -108,44 +124,45 @@ class CourseParser:
                     ()
 
             itemDict = {
-            'catalogNumber': courseCatalogNo,
+            'catalogTitle': courseCatalogNo,
             'section': courseSection,
-            'title': courseTitle
+            'courseTitle': courseTitle
             }
 
             parsed.append(itemDict)
 
         self.subjectCourses[subject] = parsed
 
+        print(f'-- parsed subject: { subject }')
+
+    def writeFiles(self):
+        self.writeMainFile()
+
+        for subjectAbbr in self.subjectCourses:
+            self.writeSubjectCoursesFile(subjectAbbr)
+
+    def writeMainFile(self):
+        semesterSubjects = f'{ self.campus.title }/{ self.catalogName }/subjects.json'
+
+        os.makedirs(os.path.dirname(semesterSubjects), exist_ok = True)
+
+        with open(semesterSubjects, "w") as subjectsFile:
+            subjectsRAW = f'{{"semester": { json.dumps(self.catalogName) }, "id": { json.dumps(self.catalogNum) }, "subjects": { json.dumps(self.subjects) }}}'
+            subjectJSON = json.loads(f'{ subjectsRAW }')
+            subjectsFile.write(json.dumps(subjectJSON, indent = 4))
+
+    def writeSubjectCoursesFile(self, subjectAbbr):
+        subjectCourses = f'{ self.campus.title }/{ self.catalogName }/{ subjectAbbr }.json'
+
+        with open(subjectCourses, "w") as subjectCoursesFile:
+            coursesRAW = f'{{"courses": { json.dumps(self.subjectCourses[subjectAbbr]) }}}'
+            coursesJSON = json.loads(coursesRAW)
+            subjectCoursesFile.write(json.dumps(coursesJSON, indent = 4))
 
 if __name__ == '__main__':
-    subjectParser = CourseParser('1224')
-    subjectParser.start()
 
-    import ipdb; ipdb.set_trace()
-
-    # html_doc = open("raw_subjects.html", "r")
-    #
-    # soup = BeautifulSoup(html_doc, 'html.parser')
-    #
-    # # Get all subjects
-    # # for part in soup.select('a[href*="class_list.html?subject"]'):
-    # #     print(part.string)
-    #
-    # cs_courses = open("cs_courses.html", "r")
-    #
-    # cs_soup = BeautifulSoup(cs_courses, 'html.parser')
-    #
-    # for part in cs_soup.select('div[class*="class-info card"]'):
-    #     header = part.findChildren("h3")[0]
-    #
-    #     courseNumber = header.findChildren("a")[0].string
-    #     spans =  header.findChildren("span")
-    #
-    #     courseSection = spans[0].string
-    #
-    #     if len(spans) < 2:
-    #         import ipdb; ipdb.set_trace()
-    #
-    #     courseTitle = spans[1].string
-    #     print(f'{courseNumber} - {courseSection} {courseTitle}')
+    for campus in allCampuses[1:2]:
+        for semester in campus.semesters:
+            subjectParser = SemesterParser(campus, semester,  campus.semesters[semester])
+            subjectParser.parseSemester()
+            subjectParser.writeFiles()
